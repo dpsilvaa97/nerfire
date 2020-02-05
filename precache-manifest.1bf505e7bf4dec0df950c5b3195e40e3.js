60bc773005692298f8e90e6481af5c1781ec40d8
dpsilvaa97 <dp.silva@campus.fct.unl.pt>
2020-02-05 16:36:48 +0000

    Updates

---
+++ precache-manifest.1bf505e7bf4dec0df950c5b3195e40e3.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37be358b4f594f64f8a433881c980fa5",
+    "url": "/nerfire/index.html"
+  },
+  {
+    "revision": "9cc31bdea49fbfa5ace8",
+    "url": "/nerfire/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "55831737f339ba430a52",
+    "url": "/nerfire/static/css/main.27f69170.chunk.css"
+  },
+  {
+    "revision": "9cc31bdea49fbfa5ace8",
+    "url": "/nerfire/static/js/2.3c71d766.chunk.js"
+  },
+  {
+    "revision": "1fedcf5c84e2f9b04a7e15ecdecddbbe",
+    "url": "/nerfire/static/js/2.3c71d766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55831737f339ba430a52",
+    "url": "/nerfire/static/js/main.6d73cb9f.chunk.js"
+  },
+  {
+    "revision": "d715294085d81f6e670c",
+    "url": "/nerfire/static/js/runtime-main.8a2e6fcd.js"
+  },
+  {
+    "revision": "61c4e0007254eb55e4691291881a4bfb",
+    "url": "/nerfire/static/media/clouds.61c4e000.png"
+  },
+  {
+    "revision": "8889176da0b4031e3a1b0de9571b06e9",
+    "url": "/nerfire/static/media/communicate.8889176d.png"
+  },
+  {
+    "revision": "c0a2a1f2a2c96231f9eaf61728c18e65",
+    "url": "/nerfire/static/media/icon.c0a2a1f2.png"
+  },
+  {
+    "revision": "4c25b657351ff761214a1993a1ef17d2",
+    "url": "/nerfire/static/media/prototype.4c25b657.png"
+  },
+  {
+    "revision": "9d0e5c6e4218ad04996a4cea8f852e70",
+    "url": "/nerfire/static/media/prototype.9d0e5c6e.png"
+  },
+  {
+    "revision": "ddbe089861dda9051e8142a9f91eaed4",
+    "url": "/nerfire/static/media/pt.ddbe0898.svg"
+  },
+  {
+    "revision": "abad056c3d8fccb67b18741080241ac3",
+    "url": "/nerfire/static/media/sensor.abad056c.png"
+  },
+  {
+    "revision": "b05d69fd80c9c953cbbba0cf000fc357",
+    "url": "/nerfire/static/media/uk.b05d69fd.svg"
+  },
+  {
+    "revision": "3e39646f53debb3f471f82037262eef2",
+    "url": "/nerfire/static/media/wallpaper1.3e39646f.jpg"
+  },
+  {
+    "revision": "a92217c24261c53c67b2dea9ec11fa6a",
+    "url": "/nerfire/static/media/world.a92217c2.png"
+  }
+]);
